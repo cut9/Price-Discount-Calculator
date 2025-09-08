0d416c035de3be47bee82e54c717601d92f6d0eb
Igor <igor.cute9@gmail.com>
2025-09-08 04:33:55 +0300

    Добавьте файлы проекта.

---
+++ Price-Discount-Calculator/Program.cs
@@ -0,0 +1,15 @@
+﻿while (true)
+{
+    Console.WriteLine("Цена?:");
+    bool isPriceParsed = decimal.TryParse(Console.ReadLine(), out decimal price);
+    Console.WriteLine("Скидка?:");
+    bool isDiscountParsed = decimal.TryParse(Console.ReadLine(), out decimal discount);
+    if (isPriceParsed && isDiscountParsed)
+    {
+        decimal savedMoney = (price * discount) / 100;
+        decimal priceAfterDiscount = price - savedMoney;
+        Console.WriteLine($"Цена с скидкой = {priceAfterDiscount}");
+        Console.WriteLine($"Экономия = {savedMoney}");
+    }
+    Console.WriteLine("----------------------------------------");
+}
